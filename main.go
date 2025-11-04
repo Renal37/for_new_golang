c92e0edfad1040612f697eb1cc977b295a30ad23
Renal37 <62982153+Renal37@users.noreply.github.com>
2025-11-04 18:25:57 +0300

    +1 задание

--- main.go
+++ main.go
@@ -4,33 +4,57 @@ import (
 	"fmt"
 )
 
-func sumArray(numbers []int) int {
-	var sum int
-	len := len(numbers)
-	if len != 0 {
-		for _, el := range numbers {
-				sum +=el
-		}
+type Student struct {
+	name  string
+	grade []int
+}
+
+func (s *Student) AddGrade(grade int) {
+	s.grade = append(s.grade, grade)
+
+}
+
+func (s Student) GetAverage() float64 {
+	var aver float64
+	len := len(s.grade)
+	if len == 0 {
+		return 0
 	} else {
-		return sum
+		for _, el := range s.grade {
+			aver += float64(el)
+		}
 	}
-	return sum
+	return aver / float64(len)
+}
+
+func IsExcellentStudent(i float64) bool {
+	if i >= 4.5 {
+		return true
+	} else {
+		return false
+	}
+
 }
 
 func main() {
-var nums []int
-	var num int
-	
-	fmt.Println("Введите числа (для завершения введите любой нечисловой символ):")
-	
-	for {
-		_, err := fmt.Scan(&num)
-		if err != nil {
-			break
-		}
-		nums = append(nums, num)
+	var name string
+	var count int
+	fmt.Println("Введите имя студента:")
+	fmt.Scan(&name)
+	student := Student{name: name, grade: []int{}}
+	fmt.Println("Сколько оценок?")
+	fmt.Scan(&count)
+	for i := 0; i < count; i++ {
+		var grade int
+		fmt.Printf("Введите оценку %d: ", i+1)
+		fmt.Scan(&grade)
+		student.AddGrade(grade)
 	}
-	
-	fmt.Println("Исходный срез:", nums)
-	fmt.Println("Сумма:", sumArray(nums))
+	if IsExcellentStudent(student.GetAverage()) == true {
+		fmt.Println("Средне бал студента выше или равно 4.5")
+	} else {
+		fmt.Println("Средне бал студента ниже 4.5")
+	}
+	fmt.Println("Средне арифметическое число:", student.GetAverage())
+
 }
